8d870c0ea866fdcec4073b511053eda960745bfa
Danilo <hilcukdanilo@gmail.com>
2022-11-03 19:20:19 +0100

    add start fille

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+fetch(
+  'https://api.themoviedb.org/3/trending/all/day?api_key=7653694c4941db1f3bfb7af19c86b9a8&gamre'
+)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    // Error handling
+  });
