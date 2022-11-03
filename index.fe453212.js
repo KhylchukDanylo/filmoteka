9caeb04bc22df489c22685182581f4b3946a3b71
KhylchukDanylo <104724231+KhylchukDanylo@users.noreply.github.com>
2022-11-03 20:24:40 +0000

    Deploying to gh-pages from @ KhylchukDanylo/filmoteka@198e2fbd6ef73d14bbffaffea8dbad392afbecf1 🚀

--- index.fe453212.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.themoviedb.org/3/trending/all/day?api_key=7653694c4941db1f3bfb7af19c86b9a8&gamre").then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})).then((function(t){console.log(t)})).catch((function(t){}));
-//# sourceMappingURL=index.fe453212.js.map
