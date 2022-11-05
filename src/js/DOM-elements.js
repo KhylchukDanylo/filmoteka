0ba3c3fb95e0252c90a9150bca68434a4dc58c79
michael_kononenko <98155086+MichaelKononenko@users.noreply.github.com>
2022-11-05 18:36:37 +0300

    first version of trailers (#15)

---
+++ src/js/DOM-elements.js
@@ -0,0 +1,6 @@
+export const refs = {
+ trailerBackdrop:document.querySelector('.trailer__backdrop'),
+ trailerModal:document.querySelector('.trailer__modal'),
+ movieList:document.querySelector('.movie'),
+ trailerFrame:document.querySelector('.trailer__frame'),
+}
