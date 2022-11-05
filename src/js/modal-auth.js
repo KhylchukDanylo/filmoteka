23748c81dbea8545d585c1da254a63cf8d1e1d42
EkaterinaKononenko <104940634+EkaterinaKononenko@users.noreply.github.com>
2022-11-05 23:56:51 +0200

    auth form change api (#18)

--- src/js/modal-auth.js
+++ src/js/modal-auth.js
@@ -10,7 +10,7 @@ import {
 } from 'firebase/auth';
 import { getDatabase, ref, set } from 'firebase/database';
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: 'AIzaSyD4R5ow53nXeGgXQPCsfLs6LgP-O91YSD8',
   authDomain: 'filmoteka-24046.firebaseapp.com',
   databaseURL: 'https://filmoteka-24046-default-rtdb.firebaseio.com',
@@ -19,26 +19,37 @@ const firebaseConfig = {
   messagingSenderId: '292791059964',
   appId: '1:292791059964:web:64f61222f9751e8cb25e79',
   measurementId: 'G-VKQT91CF12',
+};*/
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDO3O5qj1fBFaBjqh7PNPAjYn5I4RWI-q4',
+  authDomain: 'filmoteka-9038d.firebaseapp.com',
+  databaseURL: 'https://filmoteka-9038d-default-rtdb.firebaseio.com',
+  projectId: 'filmoteka-9038d',
+  storageBucket: 'filmoteka-9038d.appspot.com',
+  messagingSenderId: '668791484535',
+  appId: '1:668791484535:web:6b079499ab77f0ae261672',
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const database = getDatabase(app);
+//const database = getDatabase(app);
 
 const btnLogin = document.querySelector('.button__auth__login');
 const btnLogOut = document.querySelector('.button__auth__logout');
 const btnRegister = document.querySelector('.button__auth__register');
 const input = document.querySelector('input');
 const formAuth = document.querySelector('.form__auth');
-const btnTest = document.querySelector('.btn__auth__library');
+let btnLoginGlobal = document.querySelector('.btn__auth__library');
+let btnMyLibrary = document.querySelector('.library-nav__link');
 
 btnLogin.addEventListener('click', onBtnLogin);
 btnRegister.addEventListener('click', onBtnRegister);
 input.addEventListener('input', throttle(onValidInput, 300));
 btnLogOut.addEventListener('click', onBtnLogOut);
-btnTest.addEventListener('click', onTestBtn);
+btnLoginGlobal.addEventListener('click', onLoginGlobalBtn);
 
-function onTestBtn(e) {
+function onLoginGlobalBtn(e) {
   e.preventDefault();
   formAuth.classList.remove('visually-hidden');
 }
@@ -59,24 +70,23 @@ function onBtnRegister(e) {
     //alert('One or More Extra Fields is wrong!');
     return;
   }
-  createUserWithEmailAndPassword(auth, email, password).then(userCredential => {
-    const user = userCredential.user;
-    const db = getDatabase();
-    set(ref(db, 'users/' + user.uid), {
-      email,
-      fullName,
-      lastLogin: Date.now(),
-    })
-      .then(() => {
-        formAuth.classList.add('visually-hidden');
-        Notify.success('User created succesfully!');
-        //alert('User Created!!');
-      })
-      .catch(error => {
-        Notify.failure('Error!!!');
-        //alert(error_message);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      const user = userCredential.user;
+      const db = getDatabase();
+      set(ref(db, 'users/' + user.uid), {
+        email,
+        fullName,
+        lastLogin: Date.now(),
       });
-  });
+      formAuth.classList.add('visually-hidden');
+      btnLoginGlobal.textContent = 'my library';
+      Notify.success('User created succesfully!');
+    })
+    .catch(error => {
+      Notify.failure('User with this email is already registered');
+      //alert(error_message);
+    });
 }
 
 function onBtnLogin(e) {
@@ -92,6 +102,7 @@ function onBtnLogin(e) {
   signInWithEmailAndPassword(auth, email, password)
     .then(() => {
       formAuth.classList.add('visually-hidden');
+      btnLoginGlobal.textContent = 'my library';
       Notify.success('User logged in succesfully!');
       //alert('User logged in!');
     })
@@ -116,21 +127,22 @@ function onBtnLogin(e) {
       });
   });
 }*/
-function userAuthState(e) {
-  e.preventDefault();
-  onAuthStateChanged(auth, user => {
-    if (user !== null) {
-      const uid = user.uid;
-      localStorage.setItem('uid', uid);
-      formAuth.classList.add('visually-hidden');
-      btnLogin.classList.add('visually-hidden');
-      btnLogOut.classList.remove('visually-hidden');
-      return uid;
-    } else {
-      localStorage.removeItem('uid');
-    }
-  });
-}
+
+onAuthStateChanged(auth, user => {
+  if (user !== null) {
+    const uid = user.uid;
+    localStorage.setItem('uid', uid);
+    formAuth.classList.add('visually-hidden');
+    btnRegister.classList.add('visually-hidden');
+    btnLogin.classList.add('visually-hidden');
+    btnLogOut.classList.remove('visually-hidden');
+    btnLoginGlobal.textContent = 'my library';
+    return uid;
+  } else {
+    btnLoginGlobal.classList.add('visually hidden');
+    localStorage.removeItem('uid');
+  }
+});
 
 function onBtnLogOut(e) {
   e.preventDefault();
@@ -138,6 +150,7 @@ function onBtnLogOut(e) {
     .then(() => {
       btnLogin.classList.remove('visually-hidden');
       btnLogOut.classList.add('visually-hidden');
+      btnLoginGlobal.textContent = 'Log in';
     })
     .catch(error => {
       if (error.code) {
