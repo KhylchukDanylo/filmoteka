9cb1f814b04fea2f0f7c02525e9de7bc386fd622
Danilo <hilcukdanilo@gmail.com>
2022-11-07 21:33:21 +0100

    1

--- src/js/modal-movie-render.js
+++ src/js/modal-movie-render.js
@@ -162,6 +162,7 @@ window.addEventListener('click', e => {
 window.addEventListener('keydown', e => {
   if (e.key === 'Escape' && !movieModal.classList.contains('is-hidden')) {
     closeModal();
+    console.log(e);
   }
   //костыль, позже подумаю как сделать красиво
   else if (e.key === 'Escape' && movieModal.classList.contains('is-hidden')) {
