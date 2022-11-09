6c331f0d5e0d3fdfe08eeaa27717f7d8a15e530d
Dmitry Yurov <yurov37@gmail.com>
2022-11-09 21:13:08 +0200

    add some fix

--- src/js/modal-movie-render.js
+++ src/js/modal-movie-render.js
@@ -44,8 +44,8 @@ async function openModal(id) {
     overview,
   } = resp.data;
 
-  backdrop.classList.remove('is-hidden');
-  movieModal.classList.remove('is-hidden');
+  backdrop.classList.remove('hide-modal');
+  // movieModal.classList.remove('is-hidden');
   document.body.classList.add('stop-scrolling');
 
   movieModal.innerHTML = `<div class="movie__inner">
@@ -194,10 +194,10 @@ async function openModal(id) {
     <p class="movie__about">About</p>
     <p class="movie__description">${overview}</p>
     <div class="button-wrap">
-      <button type="button" class="movie__btn-watched" id="btn-watched">
+      <button type="button" class="movie__btn movie__btn-watched" id="btn-watched">
           add to Watched
       </button>
-          <button type="button" class="movie__btn-queue" id="btn-queue">
+          <button type="button" class="movie__btn movie__btn-queue" id="btn-queue">
       add to queue
       </button>
     </div>
@@ -253,14 +253,13 @@ async function openModal(id) {
 }
 
 function closeModal() {
-  movieModal.classList.add('is-hidden');
-  backdrop.classList.add('is-hidden');
+  // movieModal.classList.add('is-hidden');
+  backdrop.classList.add('hide-modal');
 
   document.body.classList.remove('stop-scrolling');
 }
 
 window.addEventListener('click', e => {
-  // console.log(e.target);
   if (e.target === backdrop) {
     closeModal();
   }
@@ -297,69 +296,3 @@ window.addEventListener('keydown', e => {
 
 export { movieId };
 export { backdrop };
-//save original render
-// `<div class="movie__inner">
-//   <img
-//     src="${IMG_URL}${poster_path}"
-//     alt="${original_title}"
-//     class="movie__poster"
-//   />
-//   <div class="movie__info">
-//     <h2 class="movie__title">${title}</h2>
-//     <div class="movie__info-list">
-//       <ul class="movie__characters">
-//         <li>
-//           <p>Vote / Votes</p>
-//         </li>
-//         <li>
-//           <p>Popularity</p>
-//         </li>
-//         <li>
-//           <p>Original Title</p>
-//         </li>
-//         <li>
-//           <p>Genre</p>
-//         </li>
-//       </ul>
-//       <ul class="movie__data">
-//         <li>
-//           <p>
-//             <span class="movie__data-rating">${vote_average.toFixed(
-//               1
-//             )}</span>  <span class="movie__data-slash">/</span> <span class="movie__data-count">${vote_count}</span>
-//           </p>
-//         </li>
-//         <li>
-//           <p>${popularity.toFixed(1)}</p>
-//         </li>
-//         <li>
-//           <p>${original_title}</p>
-//         </li>
-//         <li>
-//           <p>${genres.map(genre => genre.name).join(', ')}</p>
-//         </li>
-//       </ul>
-//     </div>
-//     <p class="movie__about">About</p>
-//     <p class="movie__description">${overview}</p>
-//     <div class="button-wrap">
-//       <button type="button" class="movie__btn-watched">
-//           add to Watched
-//       </button>
-//           <button type="button" class="movie__btn-queue">
-//       add to queue
-//       </button>
-//     </div>
-//     <button type="button" class="movie__btn-close">
-//       X
-//     </button>
-//   </div>
-// </div>`;
-
-//original img
-
-//  <img
-//    src="${IMG_URL}${poster_path}"
-//    alt="${original_title}"
-//    class="movie__poster"
-//  />;
