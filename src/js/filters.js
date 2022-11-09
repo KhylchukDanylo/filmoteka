b03c2fe4e23feb45b7242f476f3c056a26e43146
RomannaLG1 <melnykromasja@gmail.com>
2022-11-09 22:38:09 +0200

    fixed bags filter

--- src/js/filters.js
+++ src/js/filters.js
@@ -230,61 +230,67 @@ export async function renderFiltersResult(list) {
 
   movieList.innerHTML = movies
     .map(({ id, poster, title, genres, year, rating }) => {
-      return `<li class="movie__item">
-  <a href="#" class="movie__link" id="${id}">
-  <div class="movie__wrapper">
-  <picture>
-      <source
-        media="(min-width:1200px)"
-        
-        srcset="${
-          poster ? `https://image.tmdb.org/t/p/w500${poster}` : defaultImg
-        }"
-        type="image/jpeg"
-      />
-      <source
-        media="(min-width:768px)"
-        srcset="${
-          poster ? `https://image.tmdb.org/t/p/w342/${poster}` : defaultImg
-        }"
-        type="image/jpeg"
-      />
-      <source
-        media="(max-width:767px)"
-        
-        srcset="${
-          poster ? `https://image.tmdb.org/t/p/w342/${poster}` : defaultImg
-        }"
-        type="image/jpeg"
-      />
-
-      <img
-        class="movie-image"
-        src="${
-          poster ? `https://image.tmdb.org/t/p/w500/${poster}` : defaultImg
-        }"
-        loading="lazy"
-        alt="${title}"
-        width="395"
-        height="574"
-      />
-    </picture>
-  
-  </div>
-    
-<div class="movie__text"><h3 class="movie__name">${title}</h3>
-<p class="gallery__text" data-id="${id}">${genres} | ${year}</p>
-</div>
-${
-  !rating || rating == '0.0'
-    ? `<div class="movie__rating movie__rating--grey">NA</div>`
-    : `<div class="movie__rating movie__rating--${getClassByVote(
-        rating
-      )}">${rating}</div>`
-}
-
-  </a>
-</li>`;
+      return `<li class="movie__item" id="${id}">
+            <a href="#" class="movie__link" >
+            <div class="movie__wrapper" >
+            <picture>
+                <source
+                  media="(min-width:1200px)"
+                  
+                  srcset="${
+                    poster
+                      ? `https://image.tmdb.org/t/p/w500${poster}`
+                      : defaultImg
+                  }"
+                  type="image/jpeg"
+                />
+                <source
+                  media="(min-width:768px)"
+                  srcset="${
+                    poster
+                      ? `https://image.tmdb.org/t/p/w342/${poster}`
+                      : defaultImg
+                  }"
+                  type="image/jpeg"
+                />
+                <source
+                  media="(max-width:767px)"
+                  
+                  srcset="${
+                    poster
+                      ? `https://image.tmdb.org/t/p/w342/${poster}`
+                      : defaultImg
+                  }"
+                  type="image/jpeg"
+                />
+          
+                <img
+                  class="movie-image"
+                  src="${
+                    poster
+                      ? `https://image.tmdb.org/t/p/w500/${poster}`
+                      : defaultImg
+                  }"
+                  loading="lazy"
+                  alt="${title}"
+                  width="395"
+                  height="574"
+                />
+              </picture>
+            
+            </div>
+              
+          <div class="movie__text"><h3 class="movie__name">${title}</h3>
+          <p class="movie__genre" data-id="${id}">${genres} | ${year}</p></div>
+              ${
+                !rating || rating == '0.0'
+                  ? `<div class="movie__rating movie__rating--grey">NA</div>`
+                  : `<div class="movie__rating movie__rating--${getClassByVote(
+                      rating
+                    )}">${rating}</div>`
+              }
+            </a>
+          </li>`;
     })
     .join('');
 
