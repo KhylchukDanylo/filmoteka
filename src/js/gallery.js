239b5d398f7fbd1166dc5bb244bd89e3b1a43833
Landar-S <landarsergij@gmail.com>
2022-11-10 23:31:53 +0200

    first attempt gallery

---
+++ src/js/gallery.js
@@ -0,0 +1,151 @@
+import defaultImg from '../images/437973.webp';
+import axios from 'axios';
+
+const movieList = document.querySelector('.movie');
+const containerNothing = document.querySelector('.wrap-gallery');
+const watchedList = localStorage.getItem('wached-movies');
+const parsedWatchedList = JSON.parse(watchedList);
+const queueList = localStorage.getItem('queue-movies');
+const parsedQueueList = JSON.parse(queueList);
+
+export { showWatchedList, showQueueList, parsedWatchedList, parsedQueueList };
+
+let moviesList = [];
+
+function showWatchedList(list) {
+  if (parsedWatchedList.length > 0) {
+    containerNothing.style.display = 'none';
+  } else {
+    containerNothing.style.display = 'block';
+  }
+  moviesList = [];
+  clear();
+  list.forEach(id => {
+    createMovieList(id);
+  });
+}
+
+function showQueueList(list) {
+  if (parsedQueueList.length > 0) {
+    containerNothing.style.display = 'none';
+  } else {
+    containerNothing.style.display = 'block';
+  }
+  moviesList = [];
+  clear();
+  list.forEach(id => {
+    createMovieList(id);
+  });
+}
+
+function clear() {
+  movieList.innerHTML = '';
+}
+
+showWatchedList(parsedWatchedList);
+
+async function fetchMovieById(movieId) {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=7653694c4941db1f3bfb7af19c86b9a8&language=en-US`;
+  const response = await axios(url);
+  return response;
+}
+
+async function createMovieList(id) {
+  try {
+    const response = await fetchMovieById(id);
+
+    let moviesData = {
+      id: response.data.id,
+      poster: response.data.poster_path,
+      title: response.data.original_title,
+      genres: response.data.genre_ids,
+      year: response.data?.release_date?.slice(0, 4) || 'N/A',
+      rating: response.data.vote_average.toFixed(1),
+    };
+
+    moviesList.push(moviesData);
+
+    renderMoviesCard(moviesList);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function renderMoviesCard(moviesList) {
+  const markup = moviesList
+    .map(({ id, poster, title, genres, year, rating }) => {
+      return `<li class="movie__item" id="${id}">
+        <a href="#" class="movie__link" >
+        <div class="movie__wrapper" >
+        <picture>
+            <source
+              media="(min-width:1200px)"
+              
+              srcset="${
+                poster ? `https://image.tmdb.org/t/p/w500${poster}` : defaultImg
+              }"
+              type="image/jpeg"
+            />
+            <source
+              media="(min-width:768px)"
+              srcset="${
+                poster
+                  ? `https://image.tmdb.org/t/p/w342/${poster}`
+                  : defaultImg
+              }"
+              type="image/jpeg"
+            />
+            <source
+              media="(max-width:767px)"
+              
+              srcset="${
+                poster
+                  ? `https://image.tmdb.org/t/p/w342/${poster}`
+                  : defaultImg
+              }"
+              type="image/jpeg"
+            />
+        
+            <img
+              class="movie-image"
+              src="${
+                poster
+                  ? `https://image.tmdb.org/t/p/w500/${poster}`
+                  : defaultImg
+              }"
+              loading="lazy"
+              alt="${title}"
+              width="395"
+              height="574"
+            />
+          </picture>
+        
+        </div>
+          
+        <div class="movie__text"><h3 class="movie__name">${title}</h3>
+        <p class="movie__genre" data-id="${id}">${genres} | ${year}</p></div>
+          ${
+            !rating || rating == '0.0'
+              ? `<div class="movie__rating movie__rating--grey">NA</div>`
+              : `<div class="movie__rating movie__rating--${getClassByVote(
+                  rating
+                )}">${rating}</div>`
+          }
+        
+        </a>
+        </li>`;
+    })
+    .join('');
+
+  movieList.innerHTML = markup;
+}
+
+function getClassByVote(vote) {
+  if (vote >= 7) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
