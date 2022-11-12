00d7410702d2858a44c9bfe4a42819e50d940370
S-Mariia <105200661+S-Mariia@users.noreply.github.com>
2022-11-12 17:13:27 +0200

    update filters

--- src/js/add-filters-btn.js
+++ src/js/add-filters-btn.js
@@ -1,24 +1,24 @@
 import { refs } from './DOM-elements';
 import { CURRENT_STATE } from './pagination';
 const filterDiv = refs.formFilter;
-const { addFilterButton } = refs;
+const {addFilterBtn} = refs;
 
 const filterState = JSON.parse(localStorage.getItem(CURRENT_STATE)) === "filter";
 if (filterState) {
   filterDiv.classList.remove('spawn-filter');
-  addFilterButton.textContent = 'Close filter';
+  addFilterBtn.textContent = 'Close filter';
 } 
 
-addFilterButton.addEventListener('click', onClickBtnAddFilters);
+addFilterBtn.addEventListener('click', onClickBtnAddFilters);
 
 function onClickBtnAddFilters() {
   if (filterDiv.classList.contains('spawn-filter')) {
     filterDiv.classList.remove('spawn-filter');
-    addFilterButton.textContent = 'Close filter';
+    addFilterBtn.textContent = 'Close filter';
     return;
   } else {
     filterDiv.classList.add('spawn-filter');
-    addFilterButton.textContent = 'Open filter';
+    addFilterBtn.textContent = 'Open filter';
     return;
   }
 }
